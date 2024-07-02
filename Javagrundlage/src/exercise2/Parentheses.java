a706a60be4e91bab7e7c5ff967156c36fac66dbc
CC-Student <nataly.ukr@gmail.com>
2024-07-02 14:34:52 +0200

    new exercises

---
+++ Javagrundlage/src/exercise2/Parentheses.java
@@ -0,0 +1,19 @@
+package exercise2;
+
+public class Parentheses {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 3;
+		int z = 2;
+		
+		// System.out.println((a + b) - (3 * c)); 
+		
+		
+		int result = x / (y % z);
+		
+		System.out.println(result); 
+	}
+
+}
---
+++ Javagrundlage/src/grundlage/Exercise_3test.java
@@ -0,0 +1,10 @@
+package grundlage;
+
+public class Exercise_3test {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
